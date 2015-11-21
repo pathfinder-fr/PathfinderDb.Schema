46f3b293e058c8cceee6fc8d6927cda5e255c611
styx31 <styx31@gmail.com>
2015-11-21 21:59:59 +0100

    change project assemblyinfos

--- src/PathfinderDb.Schema/Properties/AssemblyInfo.cs
+++ src/PathfinderDb.Schema/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 ﻿using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyTitle("PathfinderDb Schema")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PathfinderDb Schema Library")]
+[assembly: AssemblyDescription("PathfinderDb Project Schema Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Schema")]
-[assembly: AssemblyCopyright("Copyright © Pathfinder-Fr")]
+[assembly: AssemblyCompany("styx31@pathfinder-fr.org")]
+[assembly: AssemblyProduct("PathfinderDb Project Schema Library")]
+[assembly: AssemblyCopyright("Copyright © styx31@pathfinder-fr.org")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
